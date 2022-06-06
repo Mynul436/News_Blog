1228fa9f9f6a2dbc653c5b5f325d6a39737d6083
Mynul Islam <mynulakash436@gmail.com>
2022-06-06 21:47:18 +0600

    API

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
-const expess =require('express')
-const app=expess()
-app.listen(3001,()=>{
-    console.log("Server running on port 3001")
+const expess = require("express");
+const app = expess();
+const db = require("./models");
+
+db.sequelize.sync().then(() => {
+  app.listen(3004, () => {
+    console.log("Server running on port 3004");
+  });
 });
