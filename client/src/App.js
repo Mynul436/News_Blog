4fe946b49449aef6ff63f647b82d9e07bdf44dec
Mynul Islam <mynulakash436@gmail.com>
2022-06-08 05:56:55 +0600

    CommentSection

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,21 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
+import Post from "./pages/Post";
+
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Link to="/createpost"> Create A Post</Link>
-        <Link to="/"> Home Page</Link>
+        <div className="navbar">
+          <Link to="/createpost"> Create A Post</Link>
+          <Link to="/"> Home Page</Link>
+        </div>
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/createpost" exact component={CreatePost} />
+          <Route path="/post/:id" exact component={Post} />
         </Switch>
       </Router>
     </div>
--- server/routes/Posts.js
+++ server/routes/Posts.js
@@ -6,7 +6,11 @@ router.get("/", async (req, res) => {
   const listOfPosts = await Posts.findAll();
   res.json(listOfPosts);
 });
-
+router.get("/byId/:id", async (req, res) => {
+  const id = req.params.id;
+  const post = await Posts.findByPk(id);
+  res.json(post);
+});
 router.post("/", async (req, res) => {
   const post = req.body;
   await Posts.create(post);
