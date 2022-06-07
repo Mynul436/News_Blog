6ac0981e88843849a7b6884a8d494db8ed9d5d78
Mynul Islam <mynulakash436@gmail.com>
2022-06-07 16:13:35 +0600

    title,post,usernamee

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,27 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 function App() {
+ const [listOfPosts, setListOfPosts] = useState([]);
+ 
+
   useEffect(()=>{
-axios.get("")
+      axios.get("http://localhost:3002/posts").then((response)=>{
+      setListOfPosts(response.data);
+});
   },[])
   return (
-    <div className="App"> </div>
+    <div className="App">
+    
+      {listOfPosts.map((value, key) => {
+        return (
+          <div className="post">
+            <div className="title"> {value.title} </div>
+            <div className="body">{value.postText}</div>
+            <div className="footer">{value.username}</div>
+          </div>
+        );
+      })}{" "}
+    </div>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const postRouter = require("./routes/Posts");
 app.use("/posts", postRouter);
 
 db.sequelize.sync().then(() => {
-  app.listen(3001, () => {
-    console.log("Server running on port 3001");
+  app.listen(3002, () => {
+    console.log("Server running on port 3002");
   });
 });
