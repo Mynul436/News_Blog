8a8dbcda747c433d69956d66ade64258ceafce82
Mynul Islam <mynulakash436@gmail.com>
2022-06-07 20:14:14 +0600

    Validation

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const postRouter = require("./routes/Posts");
 app.use("/posts", postRouter);
 
 db.sequelize.sync().then(() => {
-  app.listen(3002, () => {
+  app.listen(3001, () => {
     console.log("Server running on port 3002");
   });
 });
