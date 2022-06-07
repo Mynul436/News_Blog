f08df4374c1e48429d2cf2cc258f1d79135718c6
Mynul Islam <mynulakash436@gmail.com>
2022-06-07 15:08:19 +0600

    react

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function App() {
+  useEffect(()=>{
+axios.get("")
+  },[])
+  return (
+    <div className="App"> </div>
+  );
+}
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
-const expess = require("express");
-const app = expess();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
 const db = require("./models");
 
+// Routers
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
+
 db.sequelize.sync().then(() => {
-  app.listen(3004, () => {
-    console.log("Server running on port 3004");
+  app.listen(3001, () => {
+    console.log("Server running on port 3001");
   });
 });
